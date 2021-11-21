f2d6cb9a074dae3d636758c52b198fa85317017b
Tristan Da Costa <tristandacosta@gmail.com>
2021-11-21 22:09:29 +0100

    Ex2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-1) a
-2) 15
-3) 4
-4) Voir App.java
+1) largeur : a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p
+   prefix  : a,b,e,j,k,l,f,c,d,g,m,p,h,i,n,o
+   suffixe : j,k,l,e,f,b,c,p,m,g,h,n,o,i,d,a
+2) largeur : 20,5,25,3,12,21,28,8,12,6
+   prefixe : 20,5,3,12,8,6,13,25,21,28
+   suffixe : 3,6,8,13,12,5,21,28,25,20
+   infixe  : 3,5,6,8,12,13,20,21,25,28
--- src/App.java
+++ src/App.java
@@ -4,25 +4,23 @@ public class App {
 
     public static void main(String[] args) {
 
-        Node<String> a = new Node<>("a");
-        Node<String> b = a.setLeftChild(new Node<String>("b"));
-        Node<String> d = a.setRightChild(new Node<String>("d"));
+        Node<Integer> Num20 = new Node<>(20);
+        Node<Integer> Num5 = Num20.setLeftChild(new Node<Integer>(5));
+        Node<Integer> Num25 = Num20.setRightChild(new Node<Integer>(25));
 
-        Node<String> e = b.setLeftChild(new Node<String>("e"));
-        Node<String> f = b.setRightChild(new Node<String>("f"));
-        Node<String> g = d.setLeftChild(new Node<String>("g"));
-        Node<String> i = d.setRightChild(new Node<String>("i"));
+        Node<Integer> Num3 = Num5.setLeftChild(new Node<Integer>(3));
+        Node<Integer> Num12 = Num5.setRightChild(new Node<Integer>(12));
+        Node<Integer> Num21 = Num25.setLeftChild(new Node<Integer>(21));
+        Node<Integer> Num28 = Num25.setRightChild(new Node<Integer>(28));
 
-        Node<String> j = e.setLeftChild(new Node<String>("j"));
-        Node<String> l = e.setRightChild(new Node<String>("l"));
-        Node<String> m = g.setLeftChild(new Node<String>("m"));
-        Node<String> n = i.setLeftChild(new Node<String>("n"));
-        Node<String> o = i.setRightChild(new Node<String>("o"));
+        Node<Integer> Num8 = Num12.setLeftChild(new Node<Integer>(8));
+        Node<Integer> Num13 = Num12.setRightChild(new Node<Integer>(13));
 
-        Node<String> p = m.setLeftChild(new Node<String>("p"));
+        Node<Integer> Num6 = Num8.setLeftChild(new Node<Integer>(6));
 
-        Tree Tree = new Tree(a);
-        printTree(a, ">");
+
+        Tree Tree = new Tree(Num20);
+        printTree(Num20, ">");
 
     }
 
